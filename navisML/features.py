c356f7574d8efa3aef82aade0d1741dee2dd5b0f
dokato <dkk33@cam.ac.uk>
2022-04-09 21:31:09 +0100

    Update features.py

--- navisML/features.py
+++ navisML/features.py
@@ -2,15 +2,15 @@ import navis
 import numpy as np
 from typing import Union
 
-def get_cbf_length(neuron : Union[navis.TreeNeuron, navis.MeshNeuron]):
+def get_cbf_length(neuron : Union[navis.TreeNeuron, navis.MeshNeuron]) -> float:
     "Compute cell body fiber length of a *neuron*"
     return navis.cell_body_fiber(neuron).cable_length
 
-def get_root_radius(neuron : Union[navis.TreeNeuron, navis.MeshNeuron]):
+def get_root_radius(neuron : Union[navis.TreeNeuron, navis.MeshNeuron]) -> float:
     "Compute root (not always soma) radius of a *neuron*"
     return float(neuron.nodes[neuron.nodes.type == 'root'].radius)
 
-def get_average_radius(neuron : Union[navis.TreeNeuron, navis.MeshNeuron]):
+def get_average_radius(neuron : Union[navis.TreeNeuron, navis.MeshNeuron]) -> float:
     "Compute average radius of a *neuron*"
     return np.mean(neuron.nodes.radius)
 
